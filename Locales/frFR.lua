1884a16b73fffbddfa11095d6e1ee7ba2c780084
Nicolas Fabre <nicolas.fabre.21@outlook.fr>
2019-11-18 22:31:37 +0100

    Fix bugs

--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -1,11 +1,13 @@
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
-local L = AceLocale:NewLocale("Chronicles - Wars", "frFR", false);
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("Chronicles - Wars", "frFR", false)
 
-if not L then return end
+if not L then
+    return
+end
 
 if L then
     L["First War label"] = "La première guerre"
-    L["First War page 1"] = "<HTML><BODY><BR/><BR/><H1 align=\"center\">Première guerre</H1></BODY></HTML>"
+    L["First War page 1"] = '<HTML><BODY><BR/><BR/><H1 align="center">Première guerre</H1></BODY></HTML>'
     L["War of the ancient label"] = "La guerre des anciens"
     L["War of the ancient page 1"] = '<HTML><BODY><BR/><BR/><H1 align="center">La guerre des anciens page 1</H1></BODY></HTML>'
     L["War of the ancient page 2"] = '<HTML><BODY><BR/><BR/><H1 align="center">La guerre des anciens page 2</H1></BODY></HTML>'
